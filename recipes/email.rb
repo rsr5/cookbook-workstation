edf18a64467fad579b45b18615a8c97e8018370a
Robin Ridler <robin.ridler@gmail.com>
2016-04-04 09:23:14 +0100

    RPM bug workaround

--- recipes/email.rb
+++ recipes/email.rb
@@ -1,12 +1,14 @@
 
 location = "#{Chef::Config['file_cache_path']}/n1.rpm"
 
-package 'n1' do
-  source location
+# calling rpm command directly to work around:
+# https://github.com/nylas/N1/issues/1832
+execute 'install n1' do
+  command "rpm -Uvh --nodeps #{location}"
   action :nothing
 end
 
 remote_file location do
   source 'https://edgehill.nylas.com/download?platform=linux-rpm'
-  notifies :install, 'package[n1]', :immediately
+  notifies :run, 'execute[install n1]', :immediately
 end
